177c6f363a958fd57b9a71b1f75448f49b8fe92d
rheno felipe <92105197+rheno911@users.noreply.github.com>
2026-02-26 02:50:01 -0600

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,28 +1,14 @@
-var manifest = {
-    name: "StormExt All Sources",
-    id: "com.rheno911.stormext",
-    version: 1,
-    language: "en",
-    type: "Movie",
-    baseUrl: "https://sflix.to"
-};
-
-function getManifest(callback) {
-    callback(JSON.stringify(manifest));
+function getManifest() {
+    return {
+        name: "StormExt All Sources",
+        id: "com.rheno911.stormext",
+        version: 1,
+        baseUrl: "https://sflix.to",
+        type: "Movie",
+        language: "en"
+    };
 }
-
-function getHome(callback) {
-    callback(JSON.stringify([]));
-}
-
-function search(query, callback) {
-    callback(JSON.stringify([]));
-}
-
-function load(url, callback) {
-    callback(JSON.stringify({}));
-}
-
-function loadStreams(url, callback) {
-    callback(JSON.stringify([]));
-}
+function getHome(callback) { callback(JSON.stringify([])); }
+function search(query, callback) { callback(JSON.stringify([])); }
+function load(url, callback) { callback(JSON.stringify({})); }
+function loadStreams(url, callback) { callback(JSON.stringify([])); }
