ca2f6667156d321af768d3a69ceac386303002f6
rheno felipe <92105197+rheno911@users.noreply.github.com>
2026-02-26 06:28:20 -0600

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 var mainUrl = "https://net22.cc";
 var ua = "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
 var ch = { "User-Agent": ua, "Referer": mainUrl + "/", "Accept": "text/html,*/*;q=0.8" };
-var ah = { "User-Agent": ua, "Accept": "application/json, */*", "Referer": mainUrl + "/", "X-Requested-With": "XMLHttpRequest" };
+var ah = { "User-Agent": ua, "Accept": "application/json,*/*", "Referer": mainUrl + "/", "X-Requested-With": "XMLHttpRequest" };
 
 function getManifest() {
     return {
@@ -21,7 +21,7 @@ function parseWp(data) {
         if (!Array.isArray(arr)) return out;
         for (var i = 0; i < arr.length; i++) {
             var p = arr[i];
-            var t = p.title && p.title.rendered ? p.title.rendered : "";
+            var t = (p.title && p.title.rendered) ? p.title.rendered : "";
             var u = p.link || "";
             var img = "";
             try { img = p._embedded["wp:featuredmedia"][0].source_url || ""; } catch(e) {}
@@ -33,7 +33,7 @@ function parseWp(data) {
 
 function parseHtml(html) {
     var out = [];
-    var splits = ["flw-item","item-film","movie-item","film-item","post-item","<article"];
+    var splits = ["flw-item", "item-film", "movie-item", "film-item", "post-item", "<article"];
     var blocks = [];
     for (var s = 0; s < splits.length; s++) {
         if (html.indexOf(splits[s]) > -1) { blocks = html.split(splits[s]); break; }
@@ -41,13 +41,13 @@ function parseHtml(html) {
     for (var i = 1; i < blocks.length; i++) {
         var b = blocks[i];
         var hm = b.match(/href="(https?:[^"]{5,200})"/);
-        if (!hm) hm = b.match(/href="(/[^"]{2,200})"/);
+        if (!hm) hm = b.match(/href="(\/[^"]{2,200})"/);
         var im = b.match(/data-src="(https?:[^"]{5,200})"/);
-        if (!im) im = b.match(/src="(https?:[^"]{5,200}.(?:jpg|png|webp)[^"]*)"/);
+        if (!im) im = b.match(/src="(https?:[^"]{5,200}\.(?:jpg|png|webp)[^"]*)"/);
         var nm = b.match(/title="([^"]{2,80})"/);
         if (!nm) nm = b.match(/<h[1-4][^>]*>([^<]{2,80})</);
         if (hm && nm) {
-            var href = hm[1].charAt(0) === "/" ? mainUrl + hm[1] : hm[1];
+            var href = (hm[1].charAt(0) === "/") ? mainUrl + hm[1] : hm[1];
             var title = nm[1].trim();
             if (title.length > 1) out.push({ title: title, url: href, posterUrl: im ? im[1] : "" });
         }
@@ -78,10 +78,10 @@ function search(query, callback) {
 function load(url, callback) {
     http_get(url, ch, function(s, html) {
         var tm = html.match(/property="og:title"[^>]*content="([^"]+)"/);
-        if (!tm) tm = html.match(/<h1[^>]*>([^<]+)</h1>/);
+        if (!tm) tm = html.match(/<h1[^>]*>([^<]+)<\/h1>/);
         var dm = html.match(/property="og:description"[^>]*content="([^"]+)"/);
         var im = html.match(/property="og:image"[^>]*content="([^"]+)"/);
-        var ym = html.match(/([12][0-9]{3})</span>/);
+        var ym = html.match(/([12][0-9]{3})<\/span>/);
         var idm = html.match(/data-id="([0-9]+)"/);
         if (!idm) idm = html.match(/data-post="([0-9]+)"/);
         if (!idm) idm = html.match(/postid-([0-9]+)/);
@@ -100,30 +100,31 @@ function load(url, callback) {
 function loadStreams(url, callback) {
     http_get(url, ch, function(s, html) {
         var streams = [];
-        var dm = html.match(/["'](https?:[^"']+.m3u8[^"']*)["']/);
+        var dm = html.match(/["'](https?:[^"']+\.m3u8[^"']*)["']/);
         if (dm) { callback(JSON.stringify([{ name: "Direct", url: dm[1], headers: ch }])); return; }
         var embeds = [];
         var iparts = html.split("iframe");
         for (var i = 1; i < iparts.length; i++) {
             var sm = iparts[i].match(/src="([^"]+)"/);
             if (sm && sm[1].indexOf("google") < 0 && sm[1].indexOf("facebook") < 0) {
-                embeds.push(sm[1].charAt(0) === "/" ? mainUrl + sm[1] : sm[1]);
+                var eu = (sm[1].charAt(0) === "/") ? mainUrl + sm[1] : sm[1];
+                embeds.push(eu);
             }
         }
-        var dparts = html.split('data-embed="');
+        var dparts = html.split("data-embed=\"");
         for (var j = 1; j < dparts.length; j++) {
             var ev = dparts[j].split('"')[0];
-            if (ev.indexOf(".") > -1) embeds.push(ev.charAt(0) === "/" ? mainUrl + ev : ev);
+            if (ev.indexOf(".") > -1) embeds.push((ev.charAt(0) === "/") ? mainUrl + ev : ev);
         }
         if (embeds.length === 0) { callback(JSON.stringify(streams)); return; }
         var pending = embeds.length;
         for (var k = 0; k < embeds.length; k++) {
             (function(eu) {
                 http_get(eu, { "User-Agent": ua, "Referer": url }, function(es, eh) {
-                    var vm = eh.match(/file:s*["']([^"']+.m3u8[^"']*)["']/);
-                    if (!vm) vm = eh.match(/["'](https?:[^"'s]+.m3u8[^"'s]*)["']/);
-                    var mp = eh.match(/file:s*["']([^"']+.mp4[^"']*)["']/);
-                    if (!mp) mp = eh.match(/["'](https?:[^"'s]+.mp4[^"'s]*)["']/);
+                    var vm = eh.match(/file:\s*["']([^"']+\.m3u8[^"']*)["']/);
+                    if (!vm) vm = eh.match(/["'](https?:[^"'\s]+\.m3u8[^"'\s]*)["']/);
+                    var mp = eh.match(/file:\s*["']([^"']+\.mp4[^"']*)["']/);
+                    if (!mp) mp = eh.match(/["'](https?:[^"'\s]+\.mp4[^"'\s]*)["']/);
                     if (vm) streams.push({ name: "HLS", url: vm[1], headers: { "User-Agent": ua, "Referer": eu } });
                     else if (mp) streams.push({ name: "MP4", url: mp[1], headers: { "User-Agent": ua, "Referer": eu } });
                     pending--;
